c41da83951aac31f9a2f6b75925688eceab08e35
Igor Gonçalves <igordossantos@gmail.com>
2021-11-25 02:23:13 -0300

    Trabalhando com agregação

--- class_lutas.php
+++ class_lutas.php
@@ -8,6 +8,40 @@
         private $rounds;
         private $aprovada;
 
+        private function getDesafiado() {
+            return $this->desafiado;
+        }
+
+        private function setDesafiado($desafiado) {
+            $this->desafiado = $desafiado;
+        }
+
+        private function getDesafiante() {
+            return $this->desafiante;
+        }
+
+        private function setDesafiante($desafiante) {
+            $this->desafiante = $desafiante;
+        }
+
+        private function getRounds() {
+            return $this->rounds;
+        }
+
+        private function setRounds($rounds) {
+            $this->rounds = $rounds;
+        }
+
+        private function getAprovada() {
+            return $this->aprovada;
+        }
+
+        private function setAprovada($aprovada) {
+            $this->aprovada = $aprovada;
+        }
+
+        // Metodos abstratos
+
         public function marcarLuta() {
 
         }
