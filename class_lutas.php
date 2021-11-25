0bc4a3755c820880b545081a854f99dbabae0add
Igor Gonçalves <igordossantos@gmail.com>
2021-11-25 08:38:56 -0300

    Aprendendo a fazer a agregação assistindo a aula

--- class_lutas.php
+++ class_lutas.php
@@ -3,6 +3,7 @@
 
     class Lutas implements Lutar { 
 
+        // Aqui precisa acontecer a agregação entre as classes
         private $desafiado;
         private $desafiante;
         private $rounds;
@@ -40,13 +41,44 @@
             $this->aprovada = $aprovada;
         }
 
-        // Metodos abstratos
+        // Métodos abstratos
 
-        public function marcarLuta() {
-
+        public function marcarLuta($lutador1, $lutador2) {
+            if ($lutador1->getCategoria() == $lutador2->getCategoria() && $lutador1 != $lutador2) {
+                $this->setAprovada(true);
+                $this->desafiado = $lutador1;
+                $this->desafiante = $lutador2;
+            } else {
+                $this->setAprovada(false);
+                $this->desafiado = null;
+                $this->desafiante = null;
+            }
         }
-        public function lutar() {
+        public function lutar() { // A vitoria, derrota ou empate devem ficar gravados no cartel Lutador
+            if ($this->getAprovada() == true) {
+                $this->getDesafiado(apresentarLutador()); // Callback ?
+                $this->getDesafiante(apresentarLutador());
+                $vencedor = rand(0, 2);
 
+                switch ($vencedor) {
+                    case 0:
+                        echo "Empatou";
+                        $this->getDesafiado(empatarLuta());
+                        break;
+                    case 1:
+                        echo "Venceu";
+                        $this->getDesafiado(ganharLuta());
+                        break;
+                    case 2:
+                        echo "Perdeu";
+                        $this->getDesafiado(perderLuta());
+                        break;
+                    
+                    default:
+                        echo "A luta não pode acontecer";
+                        break;
+                }
+            }
         }
     }
 ?>
